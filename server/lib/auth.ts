b25c84e4d50e0fd343219e5fdb58a67d1c1859f6
Emmanuel Oyeleke <oyelekemmanuel@gmail.com>
2026-08-02 00:37:42 +0100

    feat: configure prisma and better-auth

---
+++ server/lib/auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from "better-auth";
+import { bearer } from "better-auth/plugins";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+import { prisma } from "../prisma";
+
+export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+    requireEmailVerification: true,
+  },
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+
+  plugins: [bearer()],
+});
